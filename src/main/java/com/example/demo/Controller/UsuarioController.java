aed4a7d1b30b678c0ba43cd9166a32cc6c243a84
Taylan <taylansilva0402@gmail.com>
2026-02-23 14:50:59 -0300

    feat: criando classe repository para acesso ao db e criando versão inicial dos endpoints

--- src/main/java/com/example/demo/Controller/UsuarioController.java
+++ src/main/java/com/example/demo/Controller/UsuarioController.java
@@ -1,11 +1,34 @@
 package com.example.demo.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.Model.Usuario;
+import com.example.demo.Service.UsuarioService;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
 
+    private final UsuarioService usuarioService;
 
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public List<Usuario> getAll() {
+        return usuarioService.getAll();
+    }
+
+    @PostMapping
+    public Usuario create(@RequestBody Usuario usuario) {
+        return usuarioService.save(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable BigInteger id) {
+        usuarioService.delete(id);
+    }
 }
