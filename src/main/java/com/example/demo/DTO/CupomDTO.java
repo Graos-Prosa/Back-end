c161f500e63934439bfb008a71504dcb4700e73c
Taylan da Silva Gonzaga <taylan.gonzaga@al.infnet.edu.br>
2026-03-05 08:35:39 -0300

    feat: criando DTO de cupom

---
+++ src/main/java/com/example/demo/DTO/CupomDTO.java
@@ -0,0 +1,24 @@
+package com.example.demo.DTO;
+
+import com.example.demo.Model.Cupom;
+import com.example.demo.Model.enumCupom;
+
+import java.math.BigDecimal;
+
+public record CupomDTO (
+        Long id,
+        String codigo,
+        BigDecimal valor,
+        String tipo,
+        enumCupom estado
+) {
+    public CupomDTO(Cupom cupom) {
+        this(
+        cupom.getIdCupom(),
+        cupom.getCodigo(),
+        cupom.getValor(),
+        cupom.getTipo(),
+        cupom.getEstado()
+        );
+    }
+}
