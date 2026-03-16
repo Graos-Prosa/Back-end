3a04a9f34cb63e58dbea590c0279b330590faca1
Taylan <taylansilva0402@gmail.com>
2026-03-16 17:30:50 -0300

    feat: adicionando endpoint /me em auth

--- src/main/java/com/example/demo/Controller/AuthController.java
+++ src/main/java/com/example/demo/Controller/AuthController.java
@@ -6,13 +6,13 @@ import com.example.demo.DTO.Auth.LoginRespostaDTO;
 import com.example.demo.DTO.Auth.RegistroRequestDTO;
 import com.example.demo.DTO.Credencial.CredencialCreateDTO;
 import com.example.demo.DTO.Credencial.CredencialDTO;
+import com.example.demo.DTO.Usuario.UsuarioDTO;
+import com.example.demo.Model.Credencial;
 import com.example.demo.Service.AuthService;
 import com.example.demo.Service.CredencialService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/autenticacao")
@@ -34,4 +34,11 @@ public class AuthController {
     public LoginRespostaDTO login(@RequestBody @Valid LoginRequestDTO dto) {
         return authService.verificarAutenticidade(dto);
     }
+
+    /* decidir se isso vai ficar assim
+    @GetMapping("/eu")
+    public UsuarioDTO eu(Authentication authentication) {
+        Credencial credencial = (Credencial) authentication.getPrincipal();
+        return new UsuarioDTO(credencial.getUsuario());
+    }*/
 }
--- src/main/java/com/example/demo/Security/SecurityConfig.java
+++ src/main/java/com/example/demo/Security/SecurityConfig.java
@@ -21,12 +21,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        //desativa proteção dos endpoints
+        /*
         http.csrf(csrf -> csrf.disable()) // desativa proteção CSRF (usado em APIs)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // permite qualquer requisição
                 )
                 .httpBasic(httpBasic -> httpBasic.disable()) // desativa login basic
                 .formLogin(form -> form.disable()); // desativa tela de login
+    */
+
+        //ativa proteção dos endpoints exceto de autenticação
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/autenticacao/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
 
         return http.build();
     }
