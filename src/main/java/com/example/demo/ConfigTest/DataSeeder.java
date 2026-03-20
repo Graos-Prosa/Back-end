2831ba7a8f4317ea652b63d9265d505dd5666bfd
Richard-Alves167 <riichardd.08@gmail.com>
2026-03-20 09:27:02 -0300

    FEAT: implementa Seed Data para a entidade Produto

--- src/main/java/com/example/demo/ConfigTest/DataSeeder.java
+++ src/main/java/com/example/demo/ConfigTest/DataSeeder.java
@@ -2,12 +2,15 @@ package com.example.demo.ConfigTest;
 
 import com.example.demo.DTO.Credencial.CredencialCreateDTO;
 import com.example.demo.DTO.Endereco.EnderecoCreateDTO;
+import com.example.demo.DTO.Produto.ProdutoCreateDTO;
 import com.example.demo.DTO.Usuario.UsuarioCreateDTO;
 import com.example.demo.Model.Credencial;
 import com.example.demo.Model.Endereco;
+import com.example.demo.Model.Produto;
 import com.example.demo.Model.Usuario;
 import com.example.demo.Repository.CredencialRepository;
 import com.example.demo.Repository.EnderecoRepository;
+import com.example.demo.Repository.ProdutoRepository;
 import com.example.demo.Repository.UsuarioRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
@@ -15,6 +18,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Configuration
@@ -97,6 +101,62 @@ public class DataSeeder {
                 enderecoDTO = new EnderecoCreateDTO(4L, "Travessa do Comércio",12,"Fundos",null,"Comércio","40015-320","Salvador","BA","Brasil");
                 endereco = modelMapper.map(enderecoDTO, Endereco.class);
                 enderecoRepository.save(endereco);
+
+            }
+        };
+    }
+    @Bean
+    @Order(4)
+    CommandLineRunner seedProduto(ProdutoRepository produtoRepository, ModelMapper modelMapper) {
+        return args -> {
+            if (produtoRepository.count() == 0) {
+                ProdutoCreateDTO produtoDTO = new ProdutoCreateDTO("Balança dose certa", "Balança dose certa para café colher cor branca 25gr","https://graoseprosa.com.br/wp-content/uploads/2026/01/balanca_dose_certa_para_cafe_colher_cor_branca.jpeg","ACESSORIO",new BigDecimal("28.00"),3);
+                Produto produto = modelMapper.map(produtoDTO, Produto.class);
+                produtoRepository.save(produto);
+
+                produtoDTO = new ProdutoCreateDTO("Bule de café", "Bule de café resistente ao calor 600ml de vidro transparente com tampa","https://graoseprosa.com.br/wp-content/uploads/2026/01/WhatsApp-Image-2026-01-20-at-10.34.20.jpeg","ACESSORIO",new BigDecimal("89.00"),1);
+                produto = modelMapper.map(produtoDTO, Produto.class);
+                produtoRepository.save(produto);
+
+                produtoDTO = new ProdutoCreateDTO("Filtro de papel branco", "Filtro de papel branco V60 02 Barista Café.","https://graoseprosa.com.br/wp-content/uploads/2026/02/WhatsApp-Image-2026-02-03-at-10.11.55.jpeg","ACESSORIO",new BigDecimal("15.00"),25);
+                produto = modelMapper.map(produtoDTO, Produto.class);
+                produtoRepository.save(produto);
+
+                produtoDTO = new ProdutoCreateDTO("Moedor de Café Elétrico", "Moedor de café elétrico de cerâmica com regulagem interna recarregável.","https://graoseprosa.com.br/wp-content/uploads/2026/02/WhatsApp-Image-2026-02-03-at-09.58.22.jpeg","ACESSORIO",new BigDecimal("98.00"),1);
+                produto = modelMapper.map(produtoDTO, Produto.class);
+                produtoRepository.save(produto);
+
+                produtoDTO = new ProdutoCreateDTO("Suporte porta filtro", "Suporte porta filtro coador café V60 102 de acrílico tipo hario.","https://graoseprosa.com.br/wp-content/uploads/2026/02/WhatsApp-Image-2026-02-03-at-09.58.23.jpeg","ACESSORIO",new BigDecimal("53.00"),10);
+                produto = modelMapper.map(produtoDTO, Produto.class);
+                produtoRepository.save(produto);
+
+                produtoDTO = new ProdutoCreateDTO("Café Arábica Variedade Catuaí 144", "Café 100% arábica, da variedade Catuaí 144, produzido na Chapada Diamantina – Bahia, a 1.350 metros de altitude.\n" +
+                        "Cultivado em região de montanha, esse café se destaca pelo sabor equilibrado, aroma agradável e doçura natural.\n" +
+                        "Possui acidez suave, corpo médio e uma bebida macia, fácil de beber, muito aromática e com notas de Mel, Melaço, Licoroso e Floral.\n" +
+                        "Um café especial que agrada tanto quem está começando no mundo dos cafés especiais quanto quem já aprecia qualidade.","https://graoseprosa.com.br/wp-content/uploads/2025/12/Captura-de-tela-de-2026-01-28-11-41-38.png","CAFE",new BigDecimal("48.00"),10);
+                produto = modelMapper.map(produtoDTO, Produto.class);
+                produtoRepository.save(produto);
+
+                produtoDTO = new ProdutoCreateDTO("Café Arábica Variedade Catucai 2SL", "Café 100% arábica, da variedade Catucaí 2SL, cultivado na região do Caparaó, terroir reconhecido pela excelência em cafés de montanha.\n" +
+                        "Apresenta perfil sensorial equilibrado, com doçura natural, acidez delicada e corpo médio, resultando em uma bebida elegante, aromática e de finalização limpa. Perfil sensorial com notas de chocolate Amargo, Melaço, Caramelo e Baunilha, aroma intenso e finalização prolongada.\n" +
+                        "Um café especial que expressa a identidade e a tradição do Caparaó, ideal para quem valoriza qualidade e origem.","https://graoseprosa.com.br/wp-content/uploads/2026/01/Captura-de-tela-de-2026-01-28-11-35-54.png","CAFE",new BigDecimal("48.00"),10);
+                produto = modelMapper.map(produtoDTO, Produto.class);
+                produtoRepository.save(produto);
+
+                produtoDTO = new ProdutoCreateDTO("Café Gourmet", "Originário de Varre-Sai (RJ), este café arábica da variedade Catuaí é cultivado a 700 metros de altitude, em um terroir marcado por clima equilibrado e solos que favorecem a expressão sensorial do grão.\n" +
+                        "\n" +
+                        "A bebida apresenta acidez média, doçura bem integrada e corpo médio a alto, com perfil sensorial elegante. Destacam-se notas cítricas delicadas, nuances de caramelo, além de finalização seca e limpa, revelando um café preciso e bem definido.\n" +
+                        "\n" +
+                        "Indicado para apreciadores que valorizam cafés de origem, com identidade regional e excelente desempenho em métodos filtrados.","https://graoseprosa.com.br/wp-content/uploads/2026/02/Captura-de-tela-de-2026-02-03-11-15-56.png","CAFE",new BigDecimal("38.00"),10);
+                produto = modelMapper.map(produtoDTO, Produto.class);
+                produtoRepository.save(produto);
+
+                produtoDTO = new ProdutoCreateDTO("Canephora Variedade Robusta", "Café da espécie Canephora, cultivado no Norte do Espírito Santo, região referência nacional na produção de cafés robustas de alta qualidade.\n" +
+                        "Apresenta perfil sensorial intenso, com corpo elevado, amargor equilibrado e baixo teor de acidez, resultando em uma bebida encorpada, marcante e persistente.\n" +
+                        "Destaca-se por seu alto teor de doçura após a torra correta, possui aroma marcante e notas de chocolate 70%, aveã, caramelo e excelente cremosidade.\n" +
+                        "Um café de personalidade forte, ideal para quem aprecia uma bebida intensa, estruturada e de alto rendimento.","https://graoseprosa.com.br/wp-content/uploads/2026/01/Captura-de-tela-de-2026-01-28-11-44-46.png","CAFE",new BigDecimal("48.00"),10);
+                produto = modelMapper.map(produtoDTO, Produto.class);
+                produtoRepository.save(produto);
 
             }
         };
