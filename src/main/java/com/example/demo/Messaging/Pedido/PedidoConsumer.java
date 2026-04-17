6074d888f9911e10d7febd7fc658130ea3376593
Richard-Alves167 <riichardd.08@gmail.com>
2026-04-16 23:51:00 -0300

    FEAT: adiciona a criação de Pagamento ao ser efetuado um Pedido por meio de serviço de mensageria

--- src/main/java/com/example/demo/Messaging/Pedido/PedidoConsumer.java
+++ src/main/java/com/example/demo/Messaging/Pedido/PedidoConsumer.java
@@ -1,14 +1,21 @@
 package com.example.demo.Messaging.Pedido;
 
 import com.example.demo.DTO.Pedido.PedidoDTO;
+import com.example.demo.Service.PagamentoService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PedidoConsumer {
+    private final PagamentoService pagamentoService;
+
+    public PedidoConsumer (PagamentoService pagamentoService) {
+        this.pagamentoService = pagamentoService;
+    }
+
     @RabbitListener(queues = "pedido.fila")
     public void consumirPedido(PedidoDTO pedido) {
         System.out.println("Pedido recebido - " + pedido.toString());
-        //Adicionar criação de pagamento ao receber pedido
+        pagamentoService.save(pedido);
     }
 }
